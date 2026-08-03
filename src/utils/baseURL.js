64af6d649024994c5042e7b3d85a067d1324c207
shahzaib3425 <shahzaibjanjua25@gmail.com>
2026-08-03 18:25:03 +0500

    test2

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,7 +1,7 @@
 
 
 export const getBaseUrl = () => {
-    return 'https://scarfaura.vercel.app/'; // Development URLs
+    return 'https://scarfaura.vercel.app'; // Development URLs
     // return 'http://localhost:5000';
   };
   
