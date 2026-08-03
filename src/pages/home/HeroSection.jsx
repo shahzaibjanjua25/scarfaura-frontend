817643c1e9c47db1d224534b46b59aab6cd94f1d
shahzaib3425 <shahzaibjanjua25@gmail.com>
2026-08-04 01:01:55 +0500

    colors3

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import card1 from '../../assets/card-1.png';
 import card2 from '../../assets/card-2.png';
 import card3 from '../../assets/card-3.png';
-import './CategoriesSection.css';
+import './hero.css';
 
 // Animation variants
 const containerVariants = {
