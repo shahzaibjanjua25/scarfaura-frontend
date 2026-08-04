b8b5a907a12e825215c6e4abe66d51b2b0b6d994
shahzaib3425 <shahzaibjanjua25@gmail.com>
2026-08-04 21:10:06 +0500

    fix 3.2

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,10 +1,12 @@
 
 
 export const getBaseUrl = () => {
-    return 'https://scarfaura.vercel.app'; // Development URLs
-    // return 'http://localhost:5000';
-  };
-  
+  console.log("3.2 is running now");
+  return 'https://scarfaura.vercel.app'; // Development URLs
+ 
+  // return 'http://localhost:5000';
+};
+
 // utils/baseURL.js
 // export const getBaseUrl = () => "http://localhost:5000";
 
