bb1ba6c7c87ffabae9ec4e8ad8a1c534a2ec02d6
shahzaib3425 <shahzaibjanjua25@gmail.com>
2026-08-05 00:46:20 +0500

    fixing contact us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -131,7 +131,7 @@ const Navbar = () => {
                 to="/contact" 
                 className="link"
               >
-                Contact
+                Contact Us
               </Link>
             </li>
           </ul>
